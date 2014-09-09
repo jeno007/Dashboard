e1d2e55b1b93e56a0ddabfcc4b6e6a9baa6c7233
Jeno Bozoki <jeno@MacBook.local>
2014-09-09 20:45:48 +0200

    commit

--- js/script.js
+++ js/script.js
@@ -62,7 +62,7 @@
 
 	var $print_date = new Date( datastream["at"] );
         $battery.html( datastream["current_value"] );
-	$battery_last_update.html( $print_date.toLocaleTimeString("hu-HU") );
+	$battery_last_update.html( $print_date.toLocaleTimeString("en-en") );
 
         // make it live
         xively.datastream.subscribe( feedID, "Battery", function ( event , data ) {
