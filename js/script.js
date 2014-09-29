68fd1a15bf5c4d63a014fe8e811ff439842ba60c
Jeno Bozoki <jeno@MacBook.local>
2014-09-29 12:39:50 +0200

    JS

--- js/script.js
+++ js/script.js
@@ -27,7 +27,9 @@
         </section>');
   }
 
+//  function d_line ( type, name, feed_id, metric )
   d_line("temperature","TESZT NÉV","battery2", "VV");
+  d_line("temperature","2-es szenzor","Sensor2", "C");
 
   function main_func (data) {
 //  xively.feed.get (feedID, function (data) {
