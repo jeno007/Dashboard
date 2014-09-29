e17c879456ac9f294424f7612d70294bffd81619
Jeno Bozoki <jeno@MacBook.local>
2014-09-29 19:34:01 +0200

    JS

--- js/script.js
+++ js/script.js
@@ -67,7 +67,7 @@
 		concat($atdate1.toLocaleTimeString("hu-HU") ));
 
         // make it live
-        xively.datastream.subscribe( feedID, datastream.id, function ( event , data ) {
+        xively.datastream.subscribe( feedID, datastream.id, function ( did ) {
 	return function (event, data) {
           ui.fakeLoad();
 	  
@@ -77,13 +77,13 @@
 		$(".js-Sensor2-last-update").toggleClass("warn", false );
 	  }
           $(".js-Sensor2").html( datastream["current_value"] );
-	  $(".js-" + datastream.id + "-last-update").html(
+	  $(".js-" + did + "-last-update").html(
 		$atdate1.
 		toLocaleDateString("hu-HU").
 		concat(" ").
 		concat($atdate1.toLocaleTimeString("hu-HU") ));
 	}
-        }());
+        }(datastream.id));
       }
       }
 
