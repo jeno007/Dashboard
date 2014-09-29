4808a3e989fa5d8209be74abf6ff5a1b8a735627
Jeno Bozoki <jeno@MacBook.local>
2014-09-29 20:41:49 +0200

    JS

--- js/script.js
+++ js/script.js
@@ -37,8 +37,6 @@
   d_line("lamp","Aktuális fogyasztás","Power", "W");
 
   function main_func (data) {
-//  xively.feed.get (feedID, function (data) {
-    // this code is executed when we get data back from Xively
 
     var feed = data,
         datastream,
@@ -48,9 +46,7 @@
 
     for (var x = 0, len = feed.datastreams.length; x < len; x++) {
       datastream = feed.datastreams[x];
-      //value = parseInt(datastream["current_value"]);
 
-      //if ( datastream.id === "Sensor2" ) {
       if ($(".js-" + datastream.id).length != 0) {
 
 	var $atdate1 = new Date( datastream["at"] );
@@ -84,91 +80,16 @@
 				toLocaleDateString("hu-HU").
 				concat(" ").
 				concat($atdate1.toLocaleTimeString("hu-HU") ));
-		}
+		};
         }(datastream.id));
-      }
-      //}
-
-/*
-      if ( datastream.id === "Sensor1" ) {
-        var $temperature = $(".js-temperature"),
-	    $temperature_last_update = $(".js-temperature-last-update"),
-	    $atdate = new Date( datastream["at"] ),
-	    $utcdate = new Date(Date.now()),
-	    $diff = new Date($utcdate - $atdate);
-
-	if (($utcdate-$atdate) > warn_limit) {
-		$temperature_last_update.toggleClass("warn", true );
-	}
-
-        $temperature.html( datastream["current_value"] );
-	$temperature_last_update.html(
-		$atdate
-		.toLocaleDateString("hu-HU")
-		.concat(" ")
-		.concat($atdate.toLocaleTimeString("hu-HU") ));
-
-        // make it live
-        xively.datastream.subscribe( feedID, "Sensor1", function ( event , data ) {
-          ui.fakeLoad();
-	  $atdate = new Date( data["at"] );
-	  $utcdate = new Date(Date.now());
-	  if (($utcdate-$atdate) < warn_limit) {
-		$temperature_last_update.toggleClass("warn", false );
-	  }
-          $temperature.html( data["current_value"] );
-	  $temperature_last_update.html(
-		$atdate
-		.toLocaleDateString("hu-HU")
-		.concat(" ")
-		.concat($atdate.toLocaleTimeString("hu-HU") ));
-        });
-      }
-
-      if ( datastream.id === "Battery" ) {
-
-        var $battery = $(".js-battery"),
-	    $battery_last_update = $(".js-battery-last-update"),
-	    $atdateb = new Date( datastream["at"] ),
-	    $utcdate = new Date(Date.now()),
-	    $diff = new Date($utcdate - $atdateb);
-
-	if (($utcdate-$atdateb) > warn_limit) {
-		$battery_last_update.toggleClass("warn", true );
-	}
-
-        $battery.html( datastream["current_value"] );
-	$battery_last_update.html(
-		$atdateb
-		.toLocaleDateString("hu-HU")
-		.concat(" ")
-		.concat($atdateb.toLocaleTimeString("hu-HU") ));
-
-        // make it live
-        xively.datastream.subscribe( feedID, "Battery", function ( event , data ) {
-          ui.fakeLoad();
-	  $atdateb = new Date( data["at"] );
-	  $utcdate = new Date(Date.now());
-	  if (($utcdate-$atdateb) < warn_limit) {
-		$battery_last_update.toggleClass("warn", false );
-	  }
-          $battery.html( data["current_value"] );
-	  $battery_last_update.html(
-		$atdateb
-		.toLocaleDateString("hu-HU")
-		.concat(" ")
-		.concat($atdateb.toLocaleTimeString("hu-HU") ));
-        });
-      }
-    }
-*/
+      };
+  };
 
     // SHOW UI
 
     $(".app-loading").fadeOut(200, function(){
      $(".app-content-inner").addClass("open");
     });
-  };
   xively.feed.get (feedID1, main_func );
   xively.feed.get (feedID2, main_func );
   xively.feed.get (feedID3, main_func );
